79b3a6e28c6f99bec480de49c9672584fb2f783e
Shreyash Patil <107754587+shreyashap@users.noreply.github.com>
2024-07-08 14:26:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://realtime-tracker-hazel.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://realtime-tracker-hazel.vercel.app/",
     credentials: true,
   })
 );
